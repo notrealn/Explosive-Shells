b836ff164c9c023589321211c46dc8b99e6a724d
notrealn <56411504+notrealn@users.noreply.github.com>
2020-03-01 14:02:00 -0500

    Update shock-and-awe.js

--- scripts/shock-and-awe.js
+++ scripts/shock-and-awe.js
@@ -1,14 +1,14 @@
 //this is NOT the complete definition for this block! see content/blocks/scatter-silo.hjson for the stats and other properties.
 
 //create a simple shockwave effect
-const siloLaunchEffectt = newEffect(20, e => {
+const siloLaunchEffect = newEffect(20, e => {
     Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
     Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
     Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
 });
 
 //create the block type
-const siloo = extendContent(Block, "scatter-silo", {
+const shockAndAwe = extendContent(Block, "shock-and-awe", {
     //override the method to build configuration
     buildConfiguration(tile, table){
         table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
@@ -24,9 +24,26 @@ const siloo = extendContent(Block, "scatter-silo", {
             //make this effect occur at the tile location
             Effects.effect(siloLaunchEffect, tile)
 
-            //create 10 bullets at this tile's location with random rotation and velocity/lifetime
-            for(var i = 0; i < 15; i++){
-                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+            for(var i = 0; i < 50; i++){
+                 Calls.createBullet(
+                    {
+                        type: FlakBulletType,
+                        damage: 0,
+                        splashDamage: 0,
+                        speed: 2,
+                        status: {
+                            damageMultiplier: 0.1,
+                            speedMultiplier: 0.1,
+                            color: ffff00
+                        }
+                    },
+                    tile.getTeam(),
+                    tile.drawx(),
+                    tile.drawy(),
+                    Mathf.random(360),
+                    Mathf.random(1.0, 2.0),
+                    Mathf.random(0.5, 2.0)
+                );
             }
             //triggering consumption makes it use up the items it requires
             tile.entity.cons.trigger()
