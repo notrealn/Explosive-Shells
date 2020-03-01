63a8e8f667712934a3f5930771757aa67d09bae4
notrealn <56411504+notrealn@users.noreply.github.com>
2020-03-01 15:20:38 -0500

    Update scatter-silo.js

--- scripts/scatter-silo.js
+++ scripts/scatter-silo.js
@@ -1,5 +1,3 @@
-
-
 //create a simple shockwave effect
 const siloLaunchEffect = newEffect(20, e => {
     Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
@@ -9,6 +7,34 @@ const siloLaunchEffect = newEffect(20, e => {
 
 //create the block type
 const silo = extendContent(Block, "scatter-silo", {
+    //override the method to build configuration
+    buildConfiguration(tile, table){
+        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
+            //configure the tile to signal that it has been pressed (this sync on client to server)
+            tile.configure(0)
+        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
+    },
+
+    //override configure event
+    configured(tile, value){
+        //make sure this silo has the items it needs to fire
+        if(tile.entity.cons.valid()){
+            //make this effect occur at the tile location
+            Effects.effect(siloLaunchEffect, tile)
+
+            //create 10 bullets at this tile's location with random rotation and velocity/lifetime
+            for(var i = 0; i < 10; i++){
+                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
+            }
+            //triggering consumption makes it use up the items it requires
+            tile.entity.cons.trigger()
+        }
+    }
+})
+
+
+//create the block type
+const shockAndAwe = extendContent(Block, "shock-and-awe", {
     //override the method to build configuration
     buildConfiguration(tile, table){
         table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
