0b5c3bcc0cc43568e85a42ac589047d04015fb61
notrealn <56411504+notrealn@users.noreply.github.com>
2020-03-01 15:45:03 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -49,9 +49,9 @@ const shockAndAwe = extendContent(Block, "shock-and-awe", {
         if(tile.entity.cons.valid()){
             //make this effect occur at the tile location
             Effects.effect(siloLaunchEffect, tile)
-            
+
             //create 10 bullets at this tile's location with random rotation and velocity/lifetime
-            for(var i = 0; i < 5; i++){
+            for(var i = 0; i < 10; i++){
                 Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
             }
             //triggering consumption makes it use up the items it requires
